f0c28cb2b9f161d0b716bb3e3c1a1e6e6e67a2a2
zhangqing <zhang2460243010@foxmail.com>
2018-12-03 15:26:45 +0800

    监听并压缩

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@ var sass = require('gulp-sass');
 
 //搭建服务器
 var webserver = require('gulp-webserver');
+
+//压缩css
+var ycss = require('gulp-clean-css');
 //创建css
 gulp.task('sass', function() {
 
     return gulp.src('./src/scss/*.scss')
         .pipe(sass())
+        .pipe(ycss())
         .pipe(gulp.dest('./src/css'))
 
 })
@@ -16,11 +20,16 @@ gulp.task('sass', function() {
 //搭建服务器
 gulp.task('webserver', function() {
 
-    return gulp.src('src')
-        .pipe(webserver({
+        return gulp.src('src')
+            .pipe(webserver({
 
-            port: 8080,
-            open: true,
-            liverseload: true,
-        }))
+                port: 8080,
+                open: true,
+                liverseload: true,
+            }))
+    })
+    //监听sass
+gulp.task('watch', function() {
+
+    return gulp.watch('./src/scss/index.scss', gulp.series('sass'))
 })
