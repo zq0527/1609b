637616304aad814b95b0c9ccfb78f226cef54986
zhangqing <zhang2460243010@foxmail.com>
2018-12-03 15:17:08 +0800

    服务器搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 //scss的编译
 var sass = require('gulp-sass');
 
+//搭建服务器
+var webserver = require('gulp-webserver');
 //创建css
 gulp.task('sass', function() {
 
@@ -9,4 +11,16 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest('./src/css'))
 
+})
+
+//搭建服务器
+gulp.task('webserver', function() {
+
+    return gulp.src('src')
+        .pipe(webserver({
+
+            port: 8080,
+            open: true,
+            liverseload: true,
+        }))
 })
